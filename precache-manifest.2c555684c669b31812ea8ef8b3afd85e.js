0729c9a9087064b2d34ee5e3fae9deec483a5386
IvanVFedenko <ivan.v.fedenko@gmail.com>
2020-04-26 19:20:40 +0300

    Updates

---
+++ precache-manifest.2c555684c669b31812ea8ef8b3afd85e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6f4742c482b75dba9a7e9212630133",
+    "url": "/movie_project/index.html"
+  },
+  {
+    "revision": "27d23cc5812c6fbee969",
+    "url": "/movie_project/static/js/2.3d735c18.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/movie_project/static/js/2.3d735c18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5437c65996b9d976fa4",
+    "url": "/movie_project/static/js/main.0935f160.chunk.js"
+  },
+  {
+    "revision": "087f15e70d590f53dae2",
+    "url": "/movie_project/static/js/runtime-main.6485409f.js"
+  }
+]);
